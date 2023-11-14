f32dd8b482e116ac87b45d1e55b65d17110ff777
मयंक <126972040+MayankDevil@users.noreply.github.com>
2023-11-14 13:09:48 +0530

    Delete vipin directory

--- vipin/7077ExceptionHandling11.java
+++
@@ -1,9 +0,0 @@
-class Exception
-{
-    public static void main(String[] args) throws ArrayIndexOutOfBoundsException
-    {
-        int[] arr = new int[5];
-        
-        arr[10] = 50; // This statement will throw an exception
-    }
-}
